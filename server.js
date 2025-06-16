a5244b4b47dbc67906086122889ae3f7f6f8cdfe
Ajit Kulkarni <askulkarni10@gmail.com>
2025-06-16 23:11:04 +0530

    another one

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors")
 require("dotenv").config();
 
+// console.log(process.env.MONGO_URI);
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,10 +16,7 @@ const settlementRoutes = require("./routes/settlements");
 app.use("/expenses", expenseRoutes);
 app.use("/", settlementRoutes); // Add more as you go
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || 3000, () => {
       console.log("Server running...");
